5ae87582b5a4a58831a13dd28adfa561e9f9e0d0
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-05-14 19:32:45 +0530

    added kthnon_divisible

---
+++ kthnon_divisible.cpp
@@ -0,0 +1,23 @@
+//Find the Kth number which is not divisible by N
+
+#include <bits/stdc++.h>  
+using namespace std; 
+int nodivisible(int n, int k) 
+{ 
+    int find = 0; 
+    int j = 0; 
+    while (find != k) { 
+        j++; 
+        if (j % n != 0) 
+            find++; 
+    } 
+    return j; 
+} 
+
+int main() 
+{ 
+    int n = 3; 
+    int k = 6; 
+    cout << nondeivisible(n, k); 
+    return 0; 
+} 
